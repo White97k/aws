641827dea2ef48dd9485452b00c83a74a5491c4e
KelvinLee <44488127+White97k@users.noreply.github.com>
2024-03-14 17:02:53 +0800

    Add files via upload

---
+++ boto3/list_az.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import boto3
+
+region = 'ap-southeast-1'
+ec2 = boto3.client('ec2', region_name=region)
+
+
+# Retrieves availability zones only for region of the ec2 object
+response = ec2.describe_availability_zones()
+
+for zone in response['AvailabilityZones']:
+    print(zone['ZoneName'])
