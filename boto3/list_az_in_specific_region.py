1c143c63051f5cdd411f2be89b1aec8431d8db01
KelvinLee <44488127+White97k@users.noreply.github.com>
2024-03-14 17:03:52 +0800

    Update and rename list_az.py to list_az_in_specific_region.py

--- boto3/list_az_in_specific_region.py
+++ boto3/list_az_in_specific_region.py
@@ -4,7 +4,6 @@ import boto3
 region = 'ap-southeast-1'
 ec2 = boto3.client('ec2', region_name=region)
 
-
 # Retrieves availability zones only for region of the ec2 object
 response = ec2.describe_availability_zones()
 
