a6024b6d11cc858f571feaf430b25600ca9fb2c7
KelvinLee <44488127+White97k@users.noreply.github.com>
2024-03-14 16:18:38 +0800

    Update retrive_keypair.py

--- boto3/retrive_keypair.py
+++ boto3/retrive_keypair.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import boto3
 
-ec2 = boto3.client('ec2')
+# Define the region
+region = 'ap-southeast-1'  # Replace 'your-region' with your desired AWS region, e.g., 'us-west-1'
+
+ec2 = boto3.client('ec2', region_name=region)
 response = ec2.describe_key_pairs()
 
 for keypair in response['KeyPairs']:
-    print(f' {keypair["KeyName"]}')
+    print(keypair["KeyName"])
