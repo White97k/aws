2a53f6851a925748c81373b6613b3d1f57d1bc26
KelvinLee <44488127+White97k@users.noreply.github.com>
2024-03-15 15:20:10 +0800

    Update list_codedeploy_deployments_status.py

--- boto3/list_codedeploy_deployments_status.py
+++ boto3/list_codedeploy_deployments_status.py
@@ -3,6 +3,11 @@ import boto3
 from datetime import datetime, timedelta
 import time
 
+# Define the region
+region = 'ap-southeast-1'  # Replace 'your-region' with your desired AWS region, e.g., 'us-west-1'
+
+print('\n #### in progress... #### \n')
+
 def get_deployments(region):
     # Initialize CodeDeploy client with the specified region
     client = boto3.client('codedeploy', region_name=region)
@@ -32,18 +37,23 @@ def get_deployments(region):
     )
 
     # Get deployment details with deploymentInfo status
+    deployment_statuses = []
     for deployment_id in response['deployments']:
         deployment_info = client.get_deployment(deploymentId=deployment_id)
         deployment_status = deployment_info['deploymentInfo']['status']
+        application_name = deployment_info['deploymentInfo']['applicationName']
+        deployment_statuses.append(deployment_status)
+        print("{}, {}, {}".format(application_name, deployment_id, deployment_status))
 
-        print("{}, Status: {}".format(deployment_id, deployment_status))
+    return deployment_statuses
 
-
-# Define the region
-region = 'ap-southeast-1'  # Replace 'your-region' with your desired AWS region, e.g., 'us-west-1'
 
 # Loop to keep accessing deployments every 30 seconds
 while True:
-    get_deployments(region)
-    time.sleep(30)  # Sleep for 30 seconds before accessing deployments again
-    print('\n#### in progressing... ####\n')
+    statuses = get_deployments(region)
+    print('\n #### end ### \n')
+    if all(status in ['Succeeded', 'Failed', 'Stopped'] for status in statuses):
+        print('All deployments have reached a terminal state.')
+        break
+
+    time.sleep(3)  # Sleep for 30 seconds before accessing deployments again
