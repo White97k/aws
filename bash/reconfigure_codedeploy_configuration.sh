e02b526ee566e845094acc8ae8a236f0538279cc
KelvinLee <44488127+White97k@users.noreply.github.com>
2024-07-08 18:30:14 +0800

    Update reconfigure_codedeploy_configuration.sh

--- bash/reconfigure_codedeploy_configuration.sh
+++ bash/reconfigure_codedeploy_configuration.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#  Define a region
+REGION=ap-southeast-1
+
+# List out codedeploy applications with prefix
+APP=aws deploy list-applications --region $REGION --output json | jq -r '.applications[] | select(startswith("xxx"))'
 
 # Reconfigure codedeploy application deployment to CodeDeployDefault.OneAtATime
-    aws deploy update-deployment-group --application-name $ASG --current-deployment-group-name $ASG --deployment-config-name CodeDeployDefault.OneAtATime --region $REGION
+    aws deploy update-deployment-group --application-name $APP --current-deployment-group-name $APP --deployment-config-name CodeDeployDefault.OneAtATime --region $REGION
 done
